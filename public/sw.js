79b6f62073d00a79b2f89e2e2dbe39623bb5cbc6
paspulsa <paspulsa.com@gmail.com>
2026-05-30 15:54:00 +0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Dikosongkan tidak masalah, syarat PWA hanya butuh event 'fetch' ini ada
+});
